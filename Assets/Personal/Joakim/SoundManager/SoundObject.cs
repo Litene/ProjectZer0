929174ea15da4b7a03c5a10672ae004254e930b8
Joakim <karbalive@hotmail.com>
2022-11-30 15:04:14 +0100

    SoundManager_WIP_1
    - Sounds are added automatically to the game and are given a key for easy-access by putting them in a folder according to sound-type.
    -Play sound
    -Play sound at transform
    -Play sound at vector3
    -AudioSource object pooling

---
+++ Assets/Personal/Joakim/SoundManager/SoundObject.cs
@@ -0,0 +1,13 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using Unity.VisualScripting;
+// using UnityEngine;
+//
+// public class SoundObject : MonoBehaviour {
+//     private Action<SoundObject> _killAction,
+//
+//     public void Init(Action<SoundObject> killAction) {
+//         _killAction = killAction;
+//     }
+//
+// }
