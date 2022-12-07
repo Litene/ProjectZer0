c728ee6b36be96c6500c9950cfc0c5e2a29fc95d
Joe Binns <joebinns.95@gmail.com>
2022-12-07 13:58:12 +0100

    Move SimpleSnow out of Personal folder

--- Assets/Scripts/Utilities/SerializeProperty.cs
+++ Assets/Scripts/Utilities/SerializeProperty.cs
@@ -4,59 +4,56 @@ using UnityEngine;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
- 
-     [System.AttributeUsage(System.AttributeTargets.Field)]
-    public class SerializeProperty : PropertyAttribute
-    {
+
+namespace Utility
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class SerializeProperty : PropertyAttribute {
         public string PropertyName { get; private set; }
- 
-        public SerializeProperty(string propertyName)
-        {
+
+        public SerializeProperty(string propertyName) {
             PropertyName = propertyName;
         }
     }
- 
-    #if UNITY_EDITOR
+
+#if UNITY_EDITOR
     [CustomPropertyDrawer(typeof(SerializeProperty))]
-    public class SerializePropertyAttributeDrawer : PropertyDrawer
-    {
-        private PropertyInfo propertyFieldInfo = null;
- 
-        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-        {
+    public class SerializePropertyAttributeDrawer : PropertyDrawer {
+        private PropertyInfo _propertyFieldInfo = null;
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
             UnityEngine.Object target = property.serializedObject.targetObject;
- 
+
             // Find the property field using reflection, in order to get access to its getter/setter.
-            if (propertyFieldInfo == null)
-                propertyFieldInfo = target.GetType().GetProperty(((SerializeProperty)attribute).PropertyName,
-                                                     BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
- 
-            if (propertyFieldInfo != null){
- 
-                  // Retrieve the value using the property getter:
-                object value = propertyFieldInfo.GetValue(target,null);
- 
+            if (_propertyFieldInfo == null)
+                _propertyFieldInfo = target.GetType().GetProperty(((SerializeProperty)attribute).PropertyName,
+                    BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+            if (_propertyFieldInfo != null){
+
+                // Retrieve the value using the property getter:
+                object value = _propertyFieldInfo.GetValue(target,null);
+
                 // Draw the property, checking for changes:
                 EditorGUI.BeginChangeCheck();
-                value = DrawProperty(position,property.propertyType,propertyFieldInfo.PropertyType,value,label);
-     
+                value = DrawProperty(position,property.propertyType,_propertyFieldInfo.PropertyType,value,label);
+ 
                 // If any changes were detected, call the property setter:
-                if (EditorGUI.EndChangeCheck() && propertyFieldInfo != null){
-     
+                if (EditorGUI.EndChangeCheck() && _propertyFieldInfo != null){
+ 
                     // Record object state for undo:
                     Undo.RecordObject(target, "Inspector");
-     
-                    // Call property setter:
-                    propertyFieldInfo.SetValue(target,value,null);
-                }
  
+                    // Call property setter:
+                    _propertyFieldInfo.SetValue(target,value,null);
+                }
+
             }else{
                 EditorGUI.LabelField(position,"Error: could not retrieve property.");
             }
         }
- 
-        private object DrawProperty(Rect position, SerializedPropertyType propertyType, Type type, object value, GUIContent label)
-        {
+
+        private object DrawProperty(Rect position, SerializedPropertyType propertyType, Type type, object value, GUIContent label) {
             switch (propertyType) {
                 case SerializedPropertyType.Integer:
                     return EditorGUI.IntField(position,label,(int)value);
@@ -92,7 +89,6 @@ using UnityEditor;
                     throw new NotImplementedException("Unimplemented propertyType "+propertyType+".");
             }
         }
- 
     }
-    #endif
- 
+#endif
+}
---
+++ Assets/Scripts/Vane/Vane.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Vane {
+    /// <summary>
+    /// Attach this Component to any GameObject which has a Transform Rotation which represents a direction.
+    /// Recommendation: Set Gizmo 3D Icons size to zero and use the Rotate Tool for best use.
+    /// </summary>
+    public class Vane : MonoBehaviour {
+    }
+}
