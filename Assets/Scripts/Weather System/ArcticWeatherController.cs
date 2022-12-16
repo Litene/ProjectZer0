8a2295a4a2f923e03298e9f3c40a1a49f9efc34b
Joe Binns <joebinns.95@gmail.com>
2022-12-16 23:41:58 +0100

    Add ambient wind strength support to snow controller

--- Assets/Scripts/Weather System/ArcticWeatherController.cs
+++ Assets/Scripts/Weather System/ArcticWeatherController.cs
@@ -1,28 +1,30 @@
-using System;
 using UnityEngine;
 using UnityEngine.Events;
 using Utilities;
 
-[ExecuteAlways]
-public class ArcticWeatherController : MonoBehaviour {
-    [SerializeProperty("Intensity")]
-    public float _intensity;
-    public float Intensity {
-        get => _intensity;
-        set {
-            _intensity = Mathf.Clamp01(value);
-            OnIntensityChanged?.Invoke(_intensity);
+namespace WeatherSystem
+{
+    [ExecuteAlways]
+    public class ArcticWeatherController : MonoBehaviour {
+        [SerializeProperty("Intensity")]
+        public float _intensity;
+        public float Intensity {
+            get => _intensity;
+            set {
+                _intensity = Mathf.Clamp01(value);
+                OnIntensityChanged?.Invoke(_intensity);
+            }
         }
-    }
     
-    [SerializeField] public UnityEvent<float> OnIntensityChanged;
+        [SerializeField] public UnityEvent<float> OnIntensityChanged;
     
-    private const float TRANSITION_DURATION = 60f;
+        private const float TRANSITION_DURATION = 60f;
 
-    private void Update()
-    {
-        if (!Application.isPlaying) { return; }
+        private void Update()
+        {
+            if (!Application.isPlaying) { return; }
         
-        Intensity += Time.deltaTime / TRANSITION_DURATION;
+            Intensity += Time.deltaTime / TRANSITION_DURATION;
+        }
     }
 }
--- Assets/Scripts/Weather System/SnowController.cs
+++ Assets/Scripts/Weather System/SnowController.cs
@@ -2,20 +2,26 @@ using UnityEngine;
 using UnityEngine.VFX;
 using UnityEngine.VFX.Utility;
 
-[ExecuteAlways]
-public class SnowController : MonoBehaviour {
-    private VisualEffect _snowVisualEffect;
-    private readonly ExposedProperty SnowfallRateProperty = "Snowfall Rate";
-    [SerializeField] private AnimationCurve _intensityToSnowfallRateCurve;
+namespace WeatherSystem
+{
+    [ExecuteAlways]
+    public class SnowController : MonoBehaviour {
+        private VisualEffect _snowVisualEffect;
+        private readonly ExposedProperty _snowfallRateProperty = "Snowfall Rate";
+        private readonly ExposedProperty _ambientWindStrengthProperty = "Ambient Wind Strength";
+        [SerializeField] private AnimationCurve _intensityToSnowfallRateCurve;
+        [SerializeField] private AnimationCurve _intensityToAmbientWindStrengthCurve;
 
-    private void OnEnable() {
-        _snowVisualEffect = GetComponent<VisualEffect>();
-    }
+        private void OnEnable() {
+            _snowVisualEffect = GetComponent<VisualEffect>();
+        }
 
-    private int IntensityToSnowfallRate(float t) => (int)_intensityToSnowfallRateCurve.Evaluate(t);
-
-    // TODO: Check preset script that I made, as it may be useful here.
-    public void SetSnowProperties(float t) {
-        _snowVisualEffect.SetInt(SnowfallRateProperty, IntensityToSnowfallRate(t));
+        private int IntensityToSnowfallRate(float t) => (int)_intensityToSnowfallRateCurve.Evaluate(t);
+        private float IntensityToAmbientWindStrength(float t) => _intensityToAmbientWindStrengthCurve.Evaluate(t);
+        
+        public void SetSnowProperties(float t) {
+            _snowVisualEffect.SetInt(_snowfallRateProperty, IntensityToSnowfallRate(t));
+            _snowVisualEffect.SetFloat(_ambientWindStrengthProperty, IntensityToAmbientWindStrength(t));
+        }
     }
 }
