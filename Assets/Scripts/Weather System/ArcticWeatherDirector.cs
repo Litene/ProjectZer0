cb53e2eafe32931653d8640bd144aa0741135733
Joe Binns <joebinns.95@gmail.com>
2022-12-22 13:27:25 +0100

    Add working DesiredArcticWeatherState dropdown

--- Assets/Scripts/Weather System/ArcticWeatherDirector.cs
+++ Assets/Scripts/Weather System/ArcticWeatherDirector.cs
@@ -1,63 +1,75 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
-using Utilities;
 
 namespace WeatherSystem
 {
     [RequireComponent(typeof(ArcticWeatherController))]
     [ExecuteAlways]
     public class ArcticWeatherDirector : MonoBehaviour {
-        // TODO: Look into generating the enum dynamically based on ArcticWeatherStates names.
-        
-        /*
-        [SerializeProperty("ArcticWeatherState")]
-        public ArcticWeatherState _arcticWeatherState;
-        public ArcticWeatherState ArcticWeatherState {
-            get => _arcticWeatherState;
-            set {
-                _arcticWeatherState = value;
-            }
-        }
-        */
-        
-        /*
-public enum ArcticWeatherState { // TODO: Convert this to a dictionary of state-names to intensity values.
-    Clear,
-    Light,
-    Moderate,
-    Heavy
-}
-*/
-        
         //private float GetProgress(float t) => (_arcticWeatherController.Intensity - _previous.Intensity) / (_target.Intensity - _previous.Intensity);
 
         private ArcticWeatherController _arcticWeatherController;
         
-        private const float TOTAL_TRANSITION_DURATION = 60f;
+        private const float TOTAL_TRANSITION_DURATION = 15f;
 
-        private void Awake()
-        {
+        private void Awake() {
             _arcticWeatherController = GetComponent<ArcticWeatherController>();
         }
+
+        private void Start() {
+            _target = _previous = _lastDesiredArcticWeatherState = ArcticWeatherStates[0];
+        }
+
+        private List<string> ArcticWeatherStateNames {
+            get {
+                return ArcticWeatherStates.Select(arcticWeatherState => arcticWeatherState.Name).ToList();
+            }
+        }
+        [Dropdown("ArcticWeatherStateNames")] public string DesiredArcticWeatherState;
+
+        public ArcticWeatherState[] ArcticWeatherStates;
 
         private ArcticWeatherState _target;
         private ArcticWeatherState _previous;
 
+        private ArcticWeatherState _lastDesiredArcticWeatherState;
 
-        private void Update() {
-            if (!Application.isPlaying) { return; }
-            UpdateIntensity();
+        private ArcticWeatherState NameToArcticWeatherState (string name) {
+            return ArcticWeatherStates.ToList().FirstOrDefault(state => name == state.Name);
         }
 
-        private void UpdateIntensity()
-        {
-            var transitionDuration = TOTAL_TRANSITION_DURATION * Mathf.Abs(_target.Intensity - _previous.Intensity);
-            var transitionDirection = Mathf.Sign(_target.Intensity - _previous.Intensity);
-            if (transitionDuration == 0) { return; }
-            _arcticWeatherController.Intensity += transitionDirection * Time.deltaTime / transitionDuration;
+        private void UpdateArcticWeatherState() {
+            var desiredArcticWeatherState = NameToArcticWeatherState(DesiredArcticWeatherState);
+            if (desiredArcticWeatherState != _lastDesiredArcticWeatherState) {
+                SetDesiredArcticWeatherState(desiredArcticWeatherState);
+            }
+            _lastDesiredArcticWeatherState = desiredArcticWeatherState;
         }
         
-        public ArcticWeatherState[] ArcticWeatherStates;
+        private void SetDesiredArcticWeatherState(ArcticWeatherState value) { 
+            _previous = _target;
+            _target = value;
+        }
+        
+        private void Update() {
+            if (!Application.isPlaying) { return; }
+            UpdateArcticWeatherState();
+            UpdateIntensity();
+        }
+        
+        private void UpdateIntensity() {
+            var transitionDirection = Mathf.Sign(_target.Intensity - _previous.Intensity);
+            var transitionRate = Time.deltaTime / TOTAL_TRANSITION_DURATION;
+            if (Mathf.Abs(_target.Intensity - _arcticWeatherController.Intensity) <= transitionRate) {
+                _arcticWeatherController.Intensity = _target.Intensity;
+                _previous = _target;
+                return; 
+            }
+            
+            _arcticWeatherController.Intensity += transitionDirection * transitionRate;
+        }
     }
 }
 
